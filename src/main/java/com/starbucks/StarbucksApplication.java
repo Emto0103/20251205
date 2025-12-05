ce55680860620fdb40375442e4fab457354e0f32
emto0103 <emto0103@gmail.com>
2025-12-05 11:52:41 +0900

    12월 5일 프레임워크 과제

--- src/main/java/com/starbucks/StarbucksApplication.java
+++ src/main/java/com/starbucks/StarbucksApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarbucksApplication {
+public class StarbucksWebApplication {  // 클래스명 변경
 
     public static void main(String[] args) {
-        SpringApplication.run(StarbucksApplication.class, args);
+        SpringApplication.run(StarbucksWebApplication.class, args);
     }
 }
